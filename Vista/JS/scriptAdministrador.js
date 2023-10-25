712a49cb1f028f61daaa8f10425735a6941ebf4a
diego cesari <82421304+clinton231@users.noreply.github.com>
2023-10-25 16:39:03 -0300

    cambios
    cambios

--- Vista/JS/scriptAdministrador.js
+++ Vista/JS/scriptAdministrador.js
@@ -17,20 +17,20 @@ document.addEventListener("DOMContentLoaded", function() {
             label: 'Cantidad Vendida',
             data: productosMasVendidos.data,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2',
-                'rgba(54, 162, 235, 0.2',
-                'rgba(255, 206, 86, 0.2',
-                'rgba(75, 192,192, 0.2',
-                'rgba(153, 102, 255, 0.2',
-                'rgba(255, 159, 64, 0.2'
+                'rgba(255, 99, 132)',
+                'rgba(54, 162, 235)',
+                'rgba(255, 206, 86)',
+                'rgba(75, 192,192)',
+                'rgba(153, 102, 255)',
+                'rgba(255, 159, 64)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 0.2',
-                'rgba(54, 162, 235, 0.2',
-                'rgba(255, 206, 86, 0.2',
-                'rgba(75, 192,192, 0.2',
-                'rgba(153, 102, 255, 0.2',
-                'rgba(255, 159, 64, 0.2'
+                'rgba(255, 99, 132)',
+                'rgba(54, 162, 235)',
+                'rgba(255, 206, 86)',
+                'rgba(75, 192,192)',
+                'rgba(153, 102, 255)',
+                'rgba(255, 159, 64)'
             ],
             borderWidth: 1
         }]
@@ -72,8 +72,8 @@ document.addEventListener("DOMContentLoaded", function() {
         datasets: [{
             label: 'Ventas Realizadas',
             data: vendedoresDestacados.data,
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            borderColor: 'rgba(75, 192, 192, 1)',
+            backgroundColor: 'rgba(75, 192, 192)',
+            borderColor: 'rgba(75, 192, 192)',
             borderWidth: 1
         }]
     };
