b76682dac049435112f94182f27a13cf948333f2
unknown <braiansolohaga24@gmail.com>
2023-11-08 14:03:04 -0300

    Correcion

--- Controlador/dbTwo.php
+++ Controlador/dbTwo.php
@@ -2,12 +2,12 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$db = "stvent";
+$db = "stock";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=" . $db . ";charset=utf8", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connected successfully";
+    //echo "Connected successfully";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
