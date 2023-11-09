d78e4273751ef35236f6930e6ba5182d3bf95c89
diego cesari <82421304+clinton231@users.noreply.github.com>
2023-11-08 23:17:01 -0300

    arreglando cagada de abril
    ajjaaj

--- MostrarProductos.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once("./db.php");
-
-    //SELECT `id`, `descripcion` FROM `provincias` WHERE 1
-    print_r( json_encode(Leer('SELECT `ID_PRODUCTO`,  `MARCA`, `NOMBRE`,  
-    `PROD_PRECIO_VENTA`,`CANTIDAD` FROM `producto` WHERE 1')));
-
-?>
