e9e5df941fb717ce91b2b9ef794f354b7b70c46b
Abril Piriz <abrilzirip@gmail.com>
2023-11-08 23:12:47 -0300

    Trayendo cosas del inodoro
    Trayendo cosas del inodoro(by diego)

---
+++ VendedorArrayCliente.php
@@ -0,0 +1,21 @@
+<?php include 'db.php'; ?>
+
+<?php
+$keywordsCliente = [];
+$consulta = $conn->query("SELECT `ID_CLIENTE`, `ID_USUARIO_REGISTRADO`, `NOMBRE`, `APELLIDO`, `APODO`, `FECHA_ALTA`, `FECHA_BAJA` FROM `cliente`");
+
+if ($consulta->rowCount()) {
+    while ($row = $consulta->fetch(PDO::FETCH_ASSOC)) {
+
+        $keywordsCliente[] = strval($row["ID_CLIENTE"]); //strval se utiliza para convertir un valor en una cadena (string). 
+        $keywordsCliente[] = $row["NOMBRE"];
+        $keywordsCliente[] = $row["APELLIDO"];
+        $keywordsCliente[] = $row["FECHA_ALTA"];
+    }
+} else {
+    echo "No se encontraron resultados en la base de datos.";
+}
+// Convierte el array en JSON
+echo json_encode($keywordsCliente); //json_encode convierte el array en una representación JSON
+?>
+<?php $conn = null; ?>
