cbd1bc0f1ad7f7b3c5f61e813b2cb634e8b3f13b
diego <diegocesari22@gmail.com>
2023-11-22 17:48:12 -0300

    .
    .

--- Vista/AdministradorCrearUsuario.php
+++ Vista/AdministradorCrearUsuario.php
@@ -1,6 +1,7 @@
 <?php
 include '../Controlador/db.php';
 
+
 session_start();
 
 if (!isset($_SESSION['usuario']) && isset($_SESSION['perfil'])) {
@@ -71,13 +72,11 @@ $consultaSelect = $conn->query("SELECT `ID_USUARIO_REGISTRADO`, `ID_TURNO`, `NOM
   
   <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous" defer></script>
-  <script src="./JS/scriptAdminsitradorCrearUsuario.js" defer></script>
-  <script src="./JS/AccionesUsuarios.js " defer></script>
-  <script src="./JS/scriptAdministradorEditaUsuario.js" defer></script>
+  <script src="./JS/scriptAdministradorEditaUsuario.js" ></script>
   <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-  <script src="./JS/scriptAdminsitradorCrearUsuario.js" defer></script>
-  <script src="./JS/scriptAdminsitradorEditaUsuario.js" defer></script>
-  <script src="./JS/AccionesUsuarios.js" defer></script>
+  
+
+ 
 
   <title>AdministradorCrearUsuario</title>
 </head>
